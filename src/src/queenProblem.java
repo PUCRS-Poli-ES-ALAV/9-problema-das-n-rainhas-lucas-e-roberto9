ffea764b52ed0b633bbf71163b7742ec4f249f80
Roberto Luís Rezende <robertodermato@gmail.com>
2020-10-19 19:34:23 -0300

    pequenos ajustes

--- src/src/queenProblem.java
+++ src/src/queenProblem.java
@@ -1,106 +1,74 @@
-/* Java program to solve N Queen Problem using
-   backtracking */
 public class queenProblem {
 
-    /* A utility function to print solution */
-    void printSolution(int board[][])
+    void escreveSolucao(int tabuleiro[][])
     {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++)
-                System.out.print(" " + board[i][j]
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro.length; j++)
+                System.out.print(" " + tabuleiro[i][j]
                         + " ");
             System.out.println();
         }
     }
 
-    /* A utility function to check if a queen can
-       be placed on board[row][col]. Note that this
-       function is called when "col" queens are already
-       placed in columns from 0 to col -1. So we need
-       to check only left side for attacking queens */
-    boolean isSafe(int board[][], int row, int col)
+    boolean podeColocar(int tabuleiro[][], int linha, int coluna)
     {
         int i, j;
 
-        /* Check this row on left side */
-        for (i = 0; i < col; i++)
-            if (board[row][i] == 1)
+        /* Checa essa coluna do lado esquerdo */
+        for (i = 0; i < coluna; i++)
+            if (tabuleiro[linha][i] == 1)
                 return false;
 
-        /* Check upper diagonal on left side */
-        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-            if (board[i][j] == 1)
+        /* Checa diagonal superior do lado esquerdo */
+        for (i = linha, j = coluna; i >= 0 && j >= 0; i--, j--)
+            if (tabuleiro[i][j] == 1)
                 return false;
 
-        /* Check lower diagonal on left side */
-        for (i = row, j = col; j >= 0 && i < board.length; i++, j--)
-            if (board[i][j] == 1)
+        /* Checa diagonal inferior do lado esquerdo */
+        for (i = linha, j = coluna; j >= 0 && i < tabuleiro.length; i++, j--)
+            if (tabuleiro[i][j] == 1)
                 return false;
 
         return true;
     }
 
-    /* A recursive utility function to solve N
-       Queen problem */
-    boolean solveNQUtil(int board[][], int col)
+    boolean resolveNQueen(int tabuleiro[][], int coluna)
     {
-        /* base case: If all queens are placed
-           then return true */
-        if (col >= board.length)
+
+        if (coluna >= tabuleiro.length)
             return true;
 
-        /* Consider this column and try placing
-           this queen in all rows one by one */
-        for (int i = 0; i < board.length; i++) {
-            /* Check if the queen can be placed on
-               board[i][col] */
-            if (isSafe(board, i, col)) {
-                /* Place this queen in board[i][col] */
-                board[i][col] = 1;
+        for (int i = 0; i < tabuleiro.length; i++) {
+            if (podeColocar(tabuleiro, i, coluna)) {
+                tabuleiro[i][coluna] = 1;
 
-                /* recur to place rest of the queens */
-                if (solveNQUtil(board, col + 1) == true)
+                if (resolveNQueen(tabuleiro, coluna + 1) == true)
                     return true;
 
-                /* If placing queen in board[i][col]
-                   doesn't lead to a solution then
-                   remove queen from board[i][col] */
-                board[i][col] = 0; // BACKTRACK
+                tabuleiro[i][coluna] = 0; // esse é o backtracking
             }
         }
-
-        /* If the queen can not be placed in any row in
-           this colum col, then return false */
         return false;
     }
 
-    /* This function solves the N Queen problem using
-       Backtracking.  It mainly uses solveNQUtil () to
-       solve the problem. It returns false if queens
-       cannot be placed, otherwise, return true and
-       prints placement of queens in the form of 1s.
-       Please note that there may be more than one
-       solutions, this function prints one of the
-       feasible solutions.*/
-    boolean solveNQ(int N)
-    {
-        int board[][] = new int [N][N];
 
-        if (solveNQUtil(board, 0) == false) {
-            System.out.print("Solution does not exist");
+    boolean resolveNQ(int N)
+    {
+        int tabuleiro[][] = new int [N][N];
+
+        if (resolveNQueen(tabuleiro, 0) == false) {
+            System.out.print("Solução não existe");
             return false;
         }
 
-        printSolution(board);
+        escreveSolucao(tabuleiro);
         return true;
     }
 
-    // driver program to test above function
     public static void main(String args[])
     {
         queenProblem Queen = new queenProblem();
-        Queen.solveNQ(20);
+        Queen.resolveNQ(20);
     }
 }
-// This code is contributed by Abhishek Shankhadhar
 
